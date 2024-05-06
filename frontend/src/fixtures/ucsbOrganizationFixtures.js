fe1b36b72e62d47c0ba293d2f2a8d865340f1ad7
Tianle Yu <yulele28@126.com>
2024-05-06 01:41:24 -0700

    [feat] created fixtures in two locations

---
+++ frontend/src/fixtures/ucsbOrganizationFixtures.js
@@ -0,0 +1,33 @@
+const ucsbOrganizationFixtures = {
+  oneUcsbOrganization: [
+    {
+      orgCode: "ORG1",
+      orgTranslationShort: "OG1",
+      orgTranslation: "Organization 1",
+      inactive: false,
+    },
+  ],
+
+  threeUcsbOrganizations: [
+    {
+      orgCode: "ORG1",
+      orgTranslationShort: "OG1",
+      orgTranslation: "Organization 1",
+      inactive: false,
+    },
+    {
+      orgCode: "ORG2",
+      orgTranslationShort: "OG2",
+      orgTranslation: "Organization 2",
+      inactive: false,
+    },
+    {
+      orgCode: "ISFA",
+      orgTranslationShort: "ISFA",
+      orgTranslation: "International Students' Film Association",
+      inactive: true,
+    },
+  ],
+};
+
+export { ucsbOrganizationFixtures };
