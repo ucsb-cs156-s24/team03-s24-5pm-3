749a3002940c92de993bffedce883289908c6e36
Tianle Yu <yulele28@126.com>
2024-05-06 17:44:15 -0700

    [feat] added table component

---
+++ frontend/src/stories/components/UCSBOrganization/UCSBOrganizationTable.stories.js
@@ -0,0 +1,45 @@
+import React from 'react';
+import UCSBOrganizationTable from "main/components/UCSBOrganization/UCSBOrganizationTable"
+import { ucsbOrganizationFixtures } from 'fixtures/ucsbOrganizationFixtures';
+import { currentUserFixtures } from 'fixtures/currentUserFixtures';
+import { rest } from "msw";
+
+export default {
+    title: 'components/UCSBOrganization/UCSBOrganizationTable',
+    component: UCSBOrganizationTable
+};
+
+const Template = (args) => {
+    return (
+        <UCSBOrganizationTable {...args} />
+    )
+};
+
+export const Empty = Template.bind({});
+
+Empty.args = {
+    dates: []
+};
+
+export const ThreeItemsOrdinaryUser = Template.bind({});
+
+ThreeItemsOrdinaryUser.args = {
+    dates: ucsbOrganizationFixtures.threeUcsbOrganizations,
+    currentUser: currentUserFixtures.userOnly,
+};
+
+export const ThreeItemsAdminUser = Template.bind({});
+ThreeItemsAdminUser.args = {
+    dates: ucsbOrganizationFixtures.threeUcsbOrganizations,
+    currentUser: currentUserFixtures.adminUser,
+}
+
+ThreeItemsAdminUser.parameters = {
+    msw: [
+        rest.delete('/api/UCSBOrganization', (req, res, ctx) => {
+            window.alert("DELETE: " + JSON.stringify(req.url));
+            return res(ctx.status(200),ctx.json({}));
+        }),
+    ]
+};
+
