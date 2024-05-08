e41086decab5f9102c3436083d3b784eb691dba0
hylee <hylee@ucsb.edu>
2024-05-07 17:42:28 -0700

    added articlesFixtures

---
+++ frontend/src/fixtures/articlesFixtures.js
@@ -0,0 +1,39 @@
+const articlesFixtures = {
+    oneArticle: {
+        "id": 1,
+        "title": "title1",
+        "url": "url1",
+        "explanation": "explanation1",
+        "email": "email1",
+        "dateAdded": "2022-01-02T12:00:00"
+    },
+    threeArticles: [
+        {
+            "id": 1,
+            "title": "title1",
+            "url": "url1",
+            "explanation": "explanation1",
+            "email": "email1",
+            "dateAdded": "2022-01-02T12:00:00"
+        },
+        {
+            "id": 2,
+            "title": "title2",
+            "url": "url2",
+            "explanation": "explanation2",
+            "email": "email2",
+            "dateAdded": "2022-01-02T12:00:01"
+        },
+        {
+            "id": 3,
+            "title": "title3",
+            "url": "url3",
+            "explanation": "explanation3",
+            "email": "email3",
+            "dateAdded": "2022-01-02T12:00:02"
+        }
+    ]
+};
+
+
+export { articlesFixtures };
