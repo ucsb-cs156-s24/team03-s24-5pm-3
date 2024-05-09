b9088fd81e94110664f321d7b125ca5e46601b07
Aaron Rosales <arosales@ucsb.edu>
2024-05-08 17:36:49 -0700

    adding so my teammate can take a look at errors

---
+++ frontend/src/fixtures/ucsbDiningCommonsMenuItemsFixtures.js
@@ -0,0 +1,36 @@
+const ucsbDiningCommonsMenuItemFixtures = {
+    oneRestaurant:
+        [
+            {
+                "id": 1,
+                "diningCommonsCode": "DLG",
+                "name": "Pizza",
+                "station": "station 1"
+            }
+        ],
+
+    threeRestaurants: 
+        [
+            {
+                "id": 2,
+                "diningCommonsCode": "DLG",
+                "name": "Burrito",
+                "station": "Main Station 2"
+            },
+            {
+                "id": 3,
+                "diningCommonsCode": "PORT",
+                "name": "Taco",
+                "station": "Station 3"
+            },
+            {
+                "id": 4,
+                "diningCommonsCode": "DLG",
+                "name": "Quesadilla",
+                "station": "Station 4"
+            }
+
+        ]
+};
+
+export { ucsbDiningCommonsMenuItemFixtures };
