7de6677669329c2245d73cde517bf2acf1c6c378
Tyler <tmcanepa@ucsb.com>
2024-05-08 18:00:06 -0700

    components

---
+++ frontend/src/main/components/RecommendationRequest/RecommendationRequestForm.js
@@ -0,0 +1,189 @@
+import { Button, Form, Row, Col } from 'react-bootstrap';
+import { useForm } from 'react-hook-form'
+import { useNavigate } from 'react-router-dom'
+
+function RecommendationRequestForm({ initialContents, submitAction, buttonLabel = "Create" }) {
+
+    // Stryker disable all
+    const {
+        register,
+        formState: { errors },
+        handleSubmit,
+    } = useForm(
+        { defaultValues: initialContents || {}, }
+    );
+    // Stryker restore all
+
+    const navigate = useNavigate();
+
+    // For explanation, see: https://stackoverflow.com/questions/3143070/javascript-regex-iso-datetime
+    // Note that even this complex regex may still need some tweaks
+
+    // Stryker disable next-line Regex
+    const isodate_regex = /(\d{4}-[01]\d-[0-3]\dT[0-2]\d:[0-5]\d:[0-5]\d\.\d+)|(\d{4}-[01]\d-[0-3]\dT[0-2]\d:[0-5]\d:[0-5]\d)|(\d{4}-[01]\d-[0-3]\dT[0-2]\d:[0-5]\d)/i;
+
+    // Stryker disable next-line all
+    const yyyyq_regex = /((19)|(20))\d{2}[1-4]/i; // Accepts from 1900-2099 followed by 1-4.  Close enough.
+
+    return (
+
+        <Form onSubmit={handleSubmit(submitAction)}>
+
+
+            <Row>
+
+                {initialContents && (
+                    <Col>
+                        <Form.Group className="mb-3" >
+                            <Form.Label htmlFor="id">Id</Form.Label>
+                            <Form.Control
+                                data-testid="RecommendationRequestForm-id"
+                                id="id"
+                                type="text"
+                                {...register("id")}
+                                value={initialContents.id}
+                                disabled
+                            />
+                        </Form.Group>
+                    </Col>
+                )}
+
+                <Col>
+                    <Form.Group className="mb-3" >
+                        <Form.Label htmlFor="requesterEmail">RequesterEmail</Form.Label>
+                        <Form.Control
+                            data-testid="RecommendationRequestForm-requesterEmail"
+                            id="requesterEmail"
+                            type="text"
+                            isInvalid={Boolean(errors.name)}
+                            {...register("requesterEmail", {
+                                required: "requesterEmail is required.",
+                                maxLength : {
+                                    value: 30,
+                                    message: "Max length 30 characters"
+                                }
+                            })}
+                        />
+                        <Form.Control.Feedback type="invalid">
+                            {errors.requesterEmail?.message}
+                        </Form.Control.Feedback>
+                    </Form.Group>
+                </Col>
+                <Col>
+                    <Form.Group className="mb-3" >
+                        <Form.Label htmlFor="professorEmail">RequesterEmail</Form.Label>
+                        <Form.Control
+                            data-testid="RecommendationRequestForm-professorEmail"
+                            id="professorEmail"
+                            type="text"
+                            {...register("professorEmail", {
+                                required: "professorEmail is required.",
+                                maxLength : {
+                                    value: 30,
+                                    message: "Max length 30 characters"
+                                }
+                            })}
+                        />
+                        <Form.Control.Feedback type="invalid">
+                            {errors.professorEmail?.message}
+                        </Form.Control.Feedback>
+                    </Form.Group>
+                </Col>
+                <Col>
+                    <Form.Group className="mb-3" >
+                        <Form.Label htmlFor="explanation">RequesterEmail</Form.Label>
+                        <Form.Control
+                            data-testid="RecommendationRequestForm-explanation"
+                            id="explanation"
+                            type="text"
+                            {...register("explanation", {
+                                required: "explanation is required.",
+                                maxLength : {
+                                    value: 50,
+                                    message: "Max length 50 characters"
+                                }
+                            })}
+                        />
+                        <Form.Control.Feedback type="invalid">
+                            {errors.explanation?.message}
+                        </Form.Control.Feedback>
+                    </Form.Group>
+                </Col>
+                <Col>
+                    <Form.Group className="mb-3" >
+                        <Form.Label htmlFor="dateRequested">Date Requested(iso format)</Form.Label>
+                        <Form.Control
+                            data-testid="RecommendationRequestForm-dateRequested"
+                            id="dateRequested"
+                            type="datetime-local"
+                            isInvalid={Boolean(errors.dateRequested)}
+                            {...register("dateRequested", { required: true, pattern: isodate_regex })}
+                        />
+                        <Form.Control.Feedback type="invalid">
+                            {errors.dateRequested && 'dateRequested is required. '}
+                        </Form.Control.Feedback>
+                    </Form.Group>
+                </Col>
+                <Col>
+                    <Form.Group className="mb-3" >
+                        <Form.Label htmlFor="dateNeeded">Date Requested(iso format)</Form.Label>
+                        <Form.Control
+                            data-testid="RecommendationRequestForm-dateNeeded"
+                            id="dateNeeded"
+                            type="datetime-local"
+                            isInvalid={Boolean(errors.dateNeeded)}
+                            {...register("dateNeeded", { required: true, pattern: isodate_regex })}
+                        />
+                        <Form.Control.Feedback type="invalid">
+                            {errors.dateNeeded && 'dateNeeded is required. '}
+                        </Form.Control.Feedback>
+                    </Form.Group>
+                </Col>
+                <Col>
+                    <Form.Group className="mb-3">
+                        <Form.Label htmlFor="done">Done</Form.Label>
+                        <Form.Select
+                            key={new Date().getTime()} //https://stackoverflow.com/questions/60446256/how-do-i-get-my-dropdown-in-react-to-update-without-having-to-refresh
+                            data-testid={testIdPrefix + "-done"}
+                            id="done"
+                            type="text"
+                            isInvalid={Boolean(errors.inactive)}
+                            {...register("done", {
+                                required: "done is required.",
+                                // pattern: /^((true)|(false))$/g,
+                            })}
+                            >
+                            <option value=""></option>
+                            <option value="true">true</option>
+                            <option value="false">false</option>
+                        </Form.Select>
+                        <Form.Control.Feedback type="invalid">
+                        {errors.done?.message}
+                        </Form.Control.Feedback>
+                    </Form.Group>
+                </Col>
+            </Row>
+
+            <Row>
+                <Col>
+                    <Button
+                        type="submit"
+                        data-testid="RecommendationRequestForm-submit"
+                    >
+                        {buttonLabel}
+                    </Button>
+                    <Button
+                        variant="Secondary"
+                        onClick={() => navigate(-1)}
+                        data-testid="RecommendationRequestForm-cancel"
+                    >
+                        Cancel
+                    </Button>
+                </Col>
+            </Row>
+        </Form>
+
+    )
+}
+
+export default RecommendationRequestForm;
