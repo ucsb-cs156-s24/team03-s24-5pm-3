02f25d619fb8d628d8a8490222ff928117e884ff
Unics Yu <43783877+yuxiaolejs@users.noreply.github.com>
2024-05-14 05:42:30 -0700

    [fix] error in App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -17,7 +17,7 @@ import ArticlesEditPage from "main/pages/Articles/ArticlesEditPage";
 
 import PlaceholderIndexPage from "main/pages/Placeholder/PlaceholderIndexPage";
 import PlaceholderCreatePage from "main/pages/Placeholder/PlaceholderCreatePage";
-import PlaceholderEditPage from "main/pages/Placeholder/PlaceholderEditPa
+import PlaceholderEditPage from "main/pages/Placeholder/PlaceholderEditPage";
 
 import UCSBDiningCommonsMenuItemIndexPage from "main/pages/UCSBDiningCommonsMenuItem/UCSBDiningCommonsMenuItemIndexPage";
 import UCSBDiningCommonsMenuItemCreatePage from "main/pages/UCSBDiningCommonsMenuItem/UCSBDiningCommonsMenuItemCreatePage";
