0ac8ef1ffd526c0eb458597dceeb236ab1187362
Tianle Yu <yulele28@126.com>
2024-05-14 09:48:01 -0700

    [feat] everything worked but tests are broken

--- frontend/src/main/pages/MenuItemReview/MenuItemReviewEditPage.js
+++ frontend/src/main/pages/MenuItemReview/MenuItemReviewEditPage.js
@@ -1,13 +1,66 @@
 import BasicLayout from "main/layouts/BasicLayout/BasicLayout";
+import { useParams } from "react-router-dom";
+import MenuItemReviewForm from "main/components/MenuItemReview/MenuItemReviewForm";
+import { Navigate } from 'react-router-dom'
+import { useBackend, useBackendMutation } from "main/utils/useBackend";
+import { toast } from "react-toastify";
 
-export default function MenuItemReviewEditPage() {
+export default function UCSBDatesEditPage({ storybook = false }) {
+  let { id } = useParams();
 
-  // Stryker disable all : placeholder for future implementation
+  const { data: ucsbDate, _error, _status } =
+    useBackend(
+      // Stryker disable next-line all : don't test internal caching of React Query
+      [`/api/menuitemreview?id=${id}`],
+      {  // Stryker disable next-line all : GET is the default, so changing this to "" doesn't introduce a bug
+        method: "GET",
+        url: `/api/menuitemreview`,
+        params: {
+          id
+        }
+      }
+    );
+
+
+  const objectToAxiosPutParams = (ucsbDate) => ({
+    url: "/api/menuitemreview",
+    method: "PUT",
+    params: {
+      id: ucsbDate.id,
+    },
+    data: ucsbDate
+  });
+
+  const onSuccess = (ucsbDate) => {
+    toast(`UCSBDate Updated - id: ${ucsbDate.id} name: ${ucsbDate.name}`);
+  }
+
+  const mutation = useBackendMutation(
+    objectToAxiosPutParams,
+    { onSuccess },
+    // Stryker disable next-line all : hard to set up test for caching
+    [`/api/menuitemreview?id=${id}`]
+  );
+
+  const { isSuccess } = mutation
+
+  const onSubmit = async (data) => {
+    mutation.mutate(data);
+  }
+
+  if (isSuccess && !storybook) {
+    return <Navigate to="/menuitemreview" />
+  }
+
   return (
     <BasicLayout>
       <div className="pt-2">
-        <h1>Edit page not yet implemented</h1>
+        <h1>Edit UCSBDate</h1>
+        {
+          ucsbDate && <MenuItemReviewForm initialContents={ucsbDate} submitAction={onSubmit} buttonLabel="Update" />
+        }
       </div>
     </BasicLayout>
   )
 }
+
