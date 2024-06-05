1dd2314dfe078a57f55e4eeb9f81489f0054118b
sherpatran <tylertran@ucsb.edu>
2024-06-04 23:41:25 -0700

    complete tests

---
+++ src/test/java/edu/ucsb/cs156/example/web/MenuItemReviewWebIT.java
@@ -0,0 +1,68 @@
+package edu.ucsb.cs156.example.web;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.DirtiesContext;
+import org.springframework.test.annotation.DirtiesContext.ClassMode;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import edu.ucsb.cs156.example.entities.MenuItemReview;
+import edu.ucsb.cs156.example.repositories.MenuItemReviewRepository;
+
+import static com.microsoft.playwright.assertions.PlaywrightAssertions.assertThat;
+
+import java.time.LocalDateTime;
+
+import edu.ucsb.cs156.example.WebTestCase;
+
+@ExtendWith(SpringExtension.class)
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.DEFINED_PORT)
+@ActiveProfiles("integration")
+@DirtiesContext(classMode = ClassMode.BEFORE_EACH_TEST_METHOD)
+public class MenuItemReviewWebIT extends WebTestCase {
+    @Autowired
+    MenuItemReviewRepository menuItemReviewRepository;
+
+    @Test
+    public void admin_user_can_create_edit_delete_menu_item_review() throws Exception {
+        setupUser(true);
+
+        LocalDateTime ldt1 = LocalDateTime.parse("2022-01-03T00:00:00");
+
+        MenuItemReview menuItemReview = MenuItemReview.builder()
+                        .itemID(1)
+                        .reviewerEmail("reviewer@gmail.com")
+                        .stars(5)
+                        .dateReviewed(ldt1)
+                        .comments("ok")
+                        .build();
+                                
+        menuItemReviewRepository.save(menuItemReview);
+
+        assertThat(page.getByTestId("MenuItemReviewTable-cell-row-0-col-comments")).hasText("ok");
+
+        page.getByTestId("MenuItemReviewTable-cell-row-0-col-Edit-button").click();
+        assertThat(page.getByText("Edit MenuItemReview")).isVisible();
+        page.getByTestId("MenuItemReviewForm-stars").fill("5");
+        page.getByTestId("MenuItemReviewForm-comments").fill("amazing");
+        page.getByTestId("MenuItemReviewForm-submit").click();
+
+        assertThat(page.getByTestId("MenuItemReviewTable-cell-row-0-col-comments")).hasText("amazing");
+
+        page.getByTestId("MenuItemReviewTable-cell-row-0-col-Delete-button").click();
+
+        assertThat(page.getByTestId("MenuItemReviewTable-cell-row-0-col-name")).not().isVisible();
+    }
+
+    @Test
+    public void regular_user_cannot_create_menu_item_review() throws Exception {
+        setupUser(false);
+
+        page.getByText("MenuItemReview").click();
+
+        assertThat(page.getByText("Create MenuItemReview")).not().isVisible();
+        assertThat(page.getByTestId("MenuItemReviewTable-cell-row-0-col-name")).not().isVisible();
+    }
+}
